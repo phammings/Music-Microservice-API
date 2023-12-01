261fb428dc4a0b5479021d5273838a3b945616d4
Ryan Pham <ryanlepham9@gmail.com>
2023-12-01 15:55:50 -0500

    Added Junit test for SongDalImpl.addSong()

--- song-microservice/src/test/java/com/eecs3311/songmicroservice/SongMicroserviceApplicationTests.java
+++ song-microservice/src/test/java/com/eecs3311/songmicroservice/SongMicroserviceApplicationTests.java
@@ -1,16 +1,33 @@
 package com.eecs3311.songmicroservice;
 
+import com.mongodb.MongoClient;
+import org.bson.types.ObjectId;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SongMicroserviceApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testAddSong_1() {
+		MongoClient mongoClient = new MongoClient();
+		MongoTemplate db = new MongoTemplate(mongoClient, "test");
+		SongDalImpl test = new SongDalImpl(db);
+
+		Song song = new Song("No More Parties in LA","Kanye West", "The Life of Pablo", "January 18th, 2016");
+		ObjectId objectId = new ObjectId();
+		song.setId(objectId);
+
+		test.addSong(song);
+		DbQueryStatus status = test.findSongById(song.getId());
+
+		assertEquals(status.getdbQueryExecResult(), DbQueryExecResult.QUERY_OK);
 	}
 
 }
