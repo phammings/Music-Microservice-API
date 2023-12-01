a5083c274ac484b377e6981a35d7eb7886636854
unknown <nafizhossain999@gmail.com>
2023-11-30 20:28:19 -0500

    added javadoc

--- profile-microservice/src/main/java/com/eecs3311/profilemicroservice/PlaylistDriverImpl.java
+++ profile-microservice/src/main/java/com/eecs3311/profilemicroservice/PlaylistDriverImpl.java
@@ -10,11 +10,24 @@ import org.neo4j.driver.v1.Transaction;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Implementation of the {@link PlaylistDriver} interface using Neo4j as the database.
+ * This class provides concrete methods to like and unlike songs in a user's playlist.
+ *
+ * <p>The class interacts with the Neo4j database using its driver and sessions to execute
+ * database operations. It encapsulates the logic to modify user's playlist preferences
+ * by liking or unliking songs.
+ */
 @Repository
 public class PlaylistDriverImpl implements PlaylistDriver {
 
 	Driver driver = ProfileMicroserviceApplication.driver;
 
+	/**
+	 * Initializes the playlist database by setting up necessary constraints.
+	 * This method is called to ensure the playlist database is properly configured
+	 * before performing any operations.
+	 */
 	public static void InitPlaylistDb() {
 		String queryStr;
 
@@ -35,6 +48,14 @@ public class PlaylistDriverImpl implements PlaylistDriver {
 		}
 	}
 
+	/**
+	 * Likes a song for a user by creating a relationship in the database.
+	 * This method adds a 'like' relation between a user's playlist and a song.
+	 *
+	 * @param userName The username of the user liking the song.
+	 * @param songId   The ID of the song to be liked.
+	 * @return {@link DbQueryStatus} indicating the result of the operation.
+	 */
 	@Override
 	public DbQueryStatus likeSong(String userName, String songId) {
 		StatementResult new_StatementResult;
@@ -74,6 +95,14 @@ public class PlaylistDriverImpl implements PlaylistDriver {
 		//return null;
 	}
 
+	/**
+	 * Unlikes a song for a user by removing a relationship in the database.
+	 * This method removes a 'like' relation between a user's playlist and a song.
+	 *
+	 * @param userName The username of the user unliking the song.
+	 * @param songId   The ID of the song to be unliked.
+	 * @return {@link DbQueryStatus} indicating the result of the operation.
+	 */
 	@Override
 	public DbQueryStatus unlikeSong(String userName, String songId) {
 		StatementResult new_StatementResult;
