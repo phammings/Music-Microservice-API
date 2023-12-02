862f2c69ad0433cc48095f315139af7060cd9f67
unknown <nafizhossain999@gmail.com>
2023-12-01 21:03:08 -0500

    Implemented testUnlikeSong_2

--- profile-microservice/src/test/java/com/eecs3311/profilemicroservice/ProfileMicroserviceApplicationTests.java
+++ profile-microservice/src/test/java/com/eecs3311/profilemicroservice/ProfileMicroserviceApplicationTests.java
@@ -168,6 +168,11 @@ public class ProfileMicroserviceApplicationTests {
 	@Test
 	public void testUnlikeSong_2() {
 		//	QUERY_ERROR_NOT_FOUND Test
+		Driver test_driver = GraphDatabase.driver("https://localhost:7687", AuthTokens.basic("neo4j", "12345678"));
+		ProfileDriverImpl test = new ProfileDriverImpl(test_driver);
+
+		DbQueryStatus status = playlistDriverImpl.unlikeSong("user", "yourPhoneLingingEatMyBlingBling");
+		assertEquals(DbQueryExecResult.QUERY_ERROR_NOT_FOUND, status.getDbQueryExecResult());
 	}
 
 	@Test
