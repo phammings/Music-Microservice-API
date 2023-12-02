e3c2fbf152107d786f18ca8d9fb24ed9449fa88b
unknown <nafizhossain999@gmail.com>
2023-12-01 20:56:59 -0500

    Implemented testGetAllSongFriendsLike_3

--- profile-microservice/src/test/java/com/eecs3311/profilemicroservice/ProfileMicroserviceApplicationTests.java
+++ profile-microservice/src/test/java/com/eecs3311/profilemicroservice/ProfileMicroserviceApplicationTests.java
@@ -116,7 +116,11 @@ public class ProfileMicroserviceApplicationTests {
 	@Test
 	public void testGetAllSongFriendsLike_3() {
 		//	QUERY_ERROR_GENERIC Test
+		Driver test_driver = GraphDatabase.driver("https://localhost:7687", AuthTokens.basic("neo4j", "12345678"));
+		ProfileDriverImpl test = new ProfileDriverImpl(test_driver);
 
+		DbQueryStatus status = profileDriverImpl.getAllSongFriendsLike("KonichiwaMeowMeow");
+		assertEquals(DbQueryExecResult.QUERY_ERROR_GENERIC, status.getDbQueryExecResult());
 	}
 
 
