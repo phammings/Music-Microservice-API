1fd2b5d62b62fd00debc6f705d0d0662cd1bd758
Ryan Pham <ryanlepham9@gmail.com>
2023-12-19 13:06:40 -0500

    Revert "removed comments from unfollowFriend used for testing phase"
    This reverts commit dde0379fdada1d45bfe03c6e4fa45cc6b6f70c9a.

--- profile-microservice/src/main/java/com/eecs3311/profilemicroservice/ProfileController.java
+++ profile-microservice/src/main/java/com/eecs3311/profilemicroservice/ProfileController.java
@@ -149,6 +149,8 @@ public class ProfileController {
 		Map<String, Object> response = new HashMap<String, Object>();
 		response.put("path", String.format("PUT %s", Utils.getUrl(request)));
 		// TODO: add any other values to the map following the example in SongController.getSongById
+
+		// Done, need to test
 		DbQueryStatus status = profileDriver.unfollowFriend(params.get(KEY_USER_NAME), params.get(KEY_FRIEND_USER_NAME));
 		response.put("msg", status.getMessage());
 		return Utils.setResponseStatus(response,status.getdbQueryExecResult(),status.getData());
